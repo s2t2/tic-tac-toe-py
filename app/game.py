0f4903b832dba89c6937359787b6231b42cc476a
MJ Rossetti <s2t2@users.noreply.github.com>
2021-12-21 19:37:09 -0500

    Resume play from preloaded state

--- app/game.py
+++ app/game.py
@@ -16,29 +16,33 @@ class Game:
             ]
 
         """
+        self.board = Board()
+
         self.players = ["X", "O"]
         self.players_cycle = cycle(self.players) # BE CAREFUL OF INFINITE LOOPS
-        self.board = Board()
         self.active_player = None
+        self.toggle_active_player() # set X as the first player
+
         self.result = None
 
         # load from pre-saved state
         self.turn_history = turn_history or []
         if any(self.turn_history):
             for player_name, square_name in self.turn_history:
-                #active_player = player_name
                 self.board.set_square(square_name, player_name)
-                #self.toggle_active_player()
+                self.toggle_active_player()
+
+
+    #def take_turn(self, player_name, square_name):
+    #    self.board.set_square(player_name, square_name)
+    #    self.toggle_active_player()
+
 
     def toggle_active_player(self):
         self.active_player = next(self.players_cycle) # https://stackoverflow.com/questions/5237611/itertools-cycle-next
 
     def play(self):
         while not self.result:
-
-            # BEGINNING OF TURN (PLAYER ALTERNATION)
-
-            self.toggle_active_player()
             print(self.board)
 
             # SQUARE SELECTION
@@ -51,6 +55,7 @@ class Game:
                 except:
                     print(f"OOPS UNRECOGNIZED SQUARE NAME '{square_name}'. PLEASE TRY AGAIN...")
                     next
+            self.toggle_active_player()
 
             # DON'T REPEAT THE PROCESS IF ANY OF THESE CONDITIONS ARE MET...
 
@@ -78,18 +83,29 @@ if __name__ == "__main__":
 
     if APP_ENV == "development":
 
-        game = Game()
-        game.play()
+        preload = input("Would you like to use a pre-saved game state? (Y/N): ")
+        if preload.upper() == "Y":
+
+            game = Game(turn_history=[
+                ("X", "A1"),
+                ("O", "A2"),
+                ("X", "B1"),
+                ("O", "B2"),
+            ])
+            game.play()
+
+        else:
+
+            game = Game()
+            game.play()
 
     else:
+
         game = Game(turn_history=[
             ("X", "A1"),
-            ("O", "C2"),
+            ("O", "A2"),
             ("X", "B1"),
             ("O", "B2"),
             ("X", "C1"),
         ])
-        print(game.board)
-        print("RESULT:", game.result)
-
-        #game.play()
+        game.winning_player
