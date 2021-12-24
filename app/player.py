3e9296b5a62382e8e6a5003265c760fbfa83522a
MJ Rossetti <s2t2@users.noreply.github.com>
2021-12-24 15:24:36 -0500

    Fix human gameplay

--- app/player.py
+++ app/player.py
@@ -16,7 +16,7 @@ class HumanPlayer(Player):
     def __init__(self, letter, name=None):
         super().__init__(name=name, letter=letter, player_type="HUMAN")
 
-    def select_square(self):
+    def select_square(self, board=None):
         return input(f"PLAYER {self.letter} PLEASE SELECT A SQUARE (i.e. 'A1'): ").upper()
 
 
