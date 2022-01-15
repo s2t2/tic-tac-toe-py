fe9392401db6c1c2325a8d1d376d590f6b8aefed
MJ Rossetti <s2t2@users.noreply.github.com>
2022-01-15 16:35:04 -0500

    gg

--- app/outcome.py
+++ app/outcome.py
@@ -4,9 +4,32 @@
 
 
 
+#from types import SimpleNamespace
+
+#class Outcome(SimpleNamespace):
 
 
-# class Outcome():
-#     def __init__(self):
-#         pass
+#class Outcome:
+#    def __init__(self):
+#        pass
 #
+#    @property
+#    def winning_letter(self):
+#        try:
+#            return self.winner["letter"]
+#        except:
+#            return None
+#
+#    @property
+#    def winning_square_names(self):
+#        try:
+#            return self.winner["square_names"]
+#        except:
+#            return None
+#
+#    @property
+#    def reason(self):
+#        try:
+#            return self.outcome["reason"]
+#        except:
+#            return None
